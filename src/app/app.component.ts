dded770fb3e873618326b9a8cc9ca7e36873d389
kckct <ss010359@gmail.com>
2017-08-05 15:44:27 +0800

    [Select Event Binding][單元測試][綠燈] 改變 property selectedValue 應該改變 property selectedValue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  cloudServices: CloudService[] = [
-    {id: '0', name: 'AWS'},
-    {id: '1', name: 'Azure'},
-    {id: '2', name: 'Aliyun'}
-  ];
-
   selectedValue = '0';
-
-  changeValue(couldSelected: HTMLInputElement) {
-    this.selectedValue = couldSelected.value;
-  }
-}
-
-export class CloudService {
-  id: string;
-  name: string;
 }
